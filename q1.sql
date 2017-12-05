fe8408c302aeb21432289ab7900db582f666b9ff
Amna Mahder Bashi <a.mahderbashi@mail.utoronto.ca>
2017-12-05 18:04:08 -0500

    updated q1.sql solution and fixed errors

--- q1.sql
+++ q1.sql
@@ -1,6 +1,19 @@
-ET SEARCH_PATH to a3;
+SET SEARCH_PATH to a3;
 
 --Report the full name and student number of all students in the database.
 -- must concatenate the first and last name, split by space and rename resulting coloumns
-SELECT fname || ' ' || lname AS FullName, id AS StudentNumber
-FROM student;
+DROP TABLE IF EXISTS q1 CASCADE; 
+CREATE TABLE q1(
+  FullName VARCHAR(1000),
+  StudentNumber BIGINT PRIMARY KEY 
+);
+
+DROP VIEW IF EXISTS allStudents CASCADE;
+CREATE VIEW allStudents AS 
+  SELECT fname || ' ' || lname AS FullName, id 
+    AS StudentNumber
+  FROM student;
+
+INSERT INTO q1 
+SELECT * 
+FROM allStudents;
