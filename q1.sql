6a0fed74c99e52c88f3792279f9dbb657966f15f
Amna Mahder Bashi <a.mahderbashi@mail.utoronto.ca>
2017-12-06 15:54:09 -0500

    removed table insertions; not needed

--- q1.sql
+++ q1.sql
@@ -2,18 +2,8 @@ SET SEARCH_PATH to a3;
 
 --Report the full name and student number of all students in the database.
 -- must concatenate the first and last name, split by space and rename resulting coloumns
-DROP TABLE IF EXISTS q1 CASCADE; 
-CREATE TABLE q1(
-  FullName VARCHAR(1000),
-  StudentNumber BIGINT PRIMARY KEY 
-);
 
-DROP VIEW IF EXISTS allStudents CASCADE;
-CREATE VIEW allStudents AS 
   SELECT fname || ' ' || lname AS FullName, id 
     AS StudentNumber
   FROM student;
 
-INSERT INTO q1 
-SELECT * 
-FROM allStudents;
