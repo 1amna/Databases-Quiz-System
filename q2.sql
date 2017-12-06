e2bf8c3c834a723afda2055046c4f5960b784d39
Amna Mahder Bashi <a.mahderbashi@mail.utoronto.ca>
2017-12-06 02:46:52 -0500

    adding first version of fully functional q2

--- q2.sql
+++ q2.sql
@@ -1,5 +1,5 @@
 SET SEARCH_PATH to a3;
-
+-- possible solution: leave count to the end, just match with the qestion id with hints first and add them to relation? 
 -- 1. For all questions in the database, report the question ID, question text, and the number of hints associated with it. For True-False questions, report NULL as the number of hints (since True-False questions cannot have hints).  
 
 DROP TABLE IF EXISTS q2 CASCADE;
@@ -18,21 +18,21 @@ CREATE TABLE q2(
 DROP VIEW IF EXISTS allQuestions CASCADE;
 -- show all questions in the bank with their individual id's and question text 
 CREATE VIEW allQuestions AS
-  SELECT id, qtexts
+  SELECT id, qtext
   FROM question;
   
 
 DROP VIEW IF EXISTS MCQ_hint_num CASCADE;
 ---number of hints per MCQ THAT HAS HINTS
 CREATE VIEW MCQ_hint_num AS
-  SELECT question_id AS id , count(answer_hint) AS counts 
-  FROM MC_hint
-  GROUP BY question_id;  
+  SELECT question.id as id, count(answer_hint) AS counts 
+  FROM MC_hint inner join question on MC_hint.question_id = question.id
+  GROUP BY question.id;  
 
 DROP VIEW IF EXISTS MC_noHint CASCADE;
 -- find the MC questions whose answers do not have hints and then assign a zero for hint count
 CREATE VIEW MC_noHint AS
-  SELECT question.id AS id, count(Null) AS counts 
+  SELECT question.id AS id,  count(Null) AS counts 
   FROM question
   WHERE qtype = 'Multiple-Choice' AND question.id NOT IN (SELECT distinct question_id FROM MC_hint)
   GROUP BY question.id;
@@ -40,9 +40,27 @@ CREATE VIEW MC_noHint AS
 DROP VIEW IF EXISTS NumQ_hint_num CASCADE;
 -- number of hints per numeric question (assuming they all do)
   CREATE VIEW NumQ_hint_num AS
-  SELECT id, count(hint) AS counts 
-  FROM numeric
-  GROUP BY id;
+  SELECT question.id as id, count(hint) AS counts 
+  FROM numeric inner join question on question.id = numeric.id
+  GROUP BY question.id;
+
+--DROP VIEW IF EXISTS TF_counts CASCADE;
+--CREATE VIEW TF_counts AS
+  --SELECT question.id AS id, NULL as counts  
+  --FROM question 
+  --WHERE qtype = 'True-False'
+  --GROUP BY question.id;
+
+DROP VIEW IF EXISTS hintsTogether CASCADE;
+CREATE VIEW hintsTogether AS
+  (SELECT * FROM NumQ_hint_num) UNION (SELECT * FROM MC_noHint) UNION (SELECT * FROM MCQ_hint_num);
+
+DROP VIEW IF EXISTS allTypes CASCADE;
+CREATE VIEW allTypes AS
+  SELECT question.id AS id, question.qtext AS qtext, hintsTogether.counts AS counts  
+  FROM question left join hintsTogether on question.id = hintsTogether.id;
+ 
+insert into q2 select * from allTypes;
 
 --DROP VIEW IF EXISTS withCountsMC CASCADE; 
 --DROP VIEW IF EXISTS withCountsNUM CASCADE;
@@ -68,6 +86,6 @@ DROP VIEW IF EXISTS NumQ_hint_num CASCADE;
 --CREATE VIEW withCountsNum AS
   -- (SELECT id,counts from allQuestions) UNION (select id, counts from MCQ_hint_num); 
    
-insert into q2 select * from withCountsNum;
+--insert into q2 select * from withCountsNum;
 
 
