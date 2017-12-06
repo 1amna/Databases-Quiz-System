9f2614e9b6b6bacfc65b10b7cc5c0d88b0acf3e7
Amna Mahder Bashi <a.mahderbashi@mail.utoronto.ca>
2017-12-05 21:39:12 -0500

    added basic file for q3.sql

---
+++ q3.sql
@@ -0,0 +1,4 @@
+SET SEARCH_PATH TO a3;
+
+--Compute the grade and total score on quiz Pr1-220310 for every student in the grade 8 class in room 120 with Mr Higgins. Report the student number, last name, and total grade.
+
